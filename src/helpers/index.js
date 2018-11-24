4e12e767bda7858c7b5a19647b64484b869c2db3
jonroby <joncaroby@gmail.com>
2018-11-24 15:18:28 -0800

    include all helpers in index

---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+const fileExists = require("./fileExists");
+const writeToFile = require("./writeToFile");
+const compose = require("./compose");
+
+module.exports = { fileExists, writeToFile, compose };
