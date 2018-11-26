c441dfea9b4efe167d7af9546ccb3b0f36c9837a
jonroby <joncaroby@gmail.com>
2018-11-26 13:35:45 -0800

    remove extra method

--- src/setup.js
+++ src/setup.js
@@ -2,20 +2,6 @@ const path = require("path");
 
 const { fileExists } = require("./helpers");
 
-function getMethods(obj) {
-  var result = [];
-  for (var id in obj) {
-    try {
-      if (typeof(obj[id]) == "function") {
-        result.push(id + ": " + obj[id].toString());
-      }
-    } catch (err) {
-      result.push(id + ": inaccessible");
-    }
-  }
-  return result;
-}
-
 const setup = () => {
   const cwd = process.cwd();
   const pathToModConfig = path.resolve(cwd, "./.mod");
